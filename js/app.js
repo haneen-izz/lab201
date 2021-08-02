cdb877cbd1b4688e7cc9a8a2cfcab5eb21e9e248
haneen <haneenjaradeh1@gmail.com>
2021-08-02 20:28:47 +0300

    code201

---
+++ js/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+ 
+let name = prompt("what's your name?").toLowerCase();
+ 
+alert("welcome to my website" +" "+ name)
+ 
+let education = prompt("do you think that i like information technology field?  yes or no ").toLowerCase();
+if(education == "yes"){
+ 
+    //console.log("yes correct")
+    alert("yes its correct")
+}
+    else if(education == "no"){
+        //console.log("its not correct")
+        alert("no its not correct")
+ 
+     
+}
+let travel = prompt("am i a good swimmer?  yes or no").toLowerCase();
+if(travel == "yes"){
+ //console.log("yes correct")
+alert("yes its correct")
+}
+else if(travel == "no"){
+   //console.log("its not correct")
+    alert("no its not correct")
+}
+ 
+let work = prompt("do you that i like work under pressure?  yes or no ").toLowerCase();
+if(work == "yes"){
+    //console.log("yes correct")
+    alert("yes its correct")
+    }
+    else if(work == "no"){
+        //console.log("its not correct")
+        alert("no its not correct")
+    }
+ 
+let language = prompt ("do you think that i like korean language?  yes or no").toLowerCase();
+if(language == "yes"){
+    //console.log("yes correct")
+    alert("yes its correct")
+    }
+    else if(language == "no"){
+     //console.log("its not correct")
+        alert("no its not correct")
+    }
+    
+let pets = prompt("do you think that i have allergic from cats?  yes or no").toLowerCase();
+if( pets == "yes"){
+     //console.log("yes correct")
+    alert("yes its correct")
+    }
+    else if(pets == "no"){
+    //console.log("its not correct")
+        alert("no its not correct")
+    }
+        alert("thanks for using my website" +" "+ name)
