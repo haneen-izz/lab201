b5113be9e67a69ad4c3263533ad565b6439d1111
haneen <haneenjaradeh1@gmail.com>
2021-08-07 21:58:40 +0300

    css style

--- js/app.js
+++ js/app.js
@@ -3,13 +3,12 @@ let name = prompt("what's your name?").toLowerCase();
  
 alert("welcome to my website" +" "+ name)
 let score = 0;
-// let education = prompt("do you think that i like information technology field?  yes or no ").toLowerCase();
  function Q1(){
  let education = prompt("do you think that i like information technology field?  yes or no ").toLowerCase();
     switch (education ) {
         case 'yes':
             case 'y':
-            //console.log('thats right');
+            console.log('thats right');
             
             alert("thats right");
             score++;
@@ -31,7 +30,7 @@ let travel = prompt("am i a good swimmer?  yes or no").toLowerCase();
 switch (travel) {
     case 'yes':
     case 'y':
-    //console.log('thats right');
+    console.log('thats right');
     alert("thats right");
     score++;
     break;
@@ -48,7 +47,7 @@ let work = prompt("do you that i like work under pressure?  yes or no ").toLower
 switch (work) {
         case 'yes':
         case 'y':
-        //console.log('thats right');
+        console.log('thats right');
         alert("thats right");
         score++;
         break;
@@ -66,7 +65,7 @@ let language = prompt ("do you think that i like korean language?  yes or no").t
 switch (language) {
     case 'yes':
         case 'y':
-        //console.log('thats right');
+        console.log('thats right');
         alert("thats right");
         score++;
         break;
@@ -84,7 +83,7 @@ let pets = prompt("do you think that i have allergic from cats?  yes or no").toL
 switch (pets) {
     case 'yes':
         case 'y':
-        //console.log('thats right');
+        console.log('thats right');
         alert("thats right");
         score++;
         break;
@@ -129,12 +128,12 @@ if(i === 4){
          
         for (let x = 0; x < 6; x++) {
             movie = (prompt("the best movie for me 'Parasite' ,'Train to Busan' , 'avatar' , 'Poetry' ,'Okja' ,'Mother' "))
-           // console.log(movie + " user answer");
+           console.log(movie + " user answer");
         
 
       let j
       for ( j = 0; j < bestMovies.length; j++) { 
-        // console.log(bestMovies.[j] + "correct answer");
+        console.log(bestMovies[j] + "correct answer");
 
         if (movie ==  bestMovies[j]) {
         alert("yes correct");
